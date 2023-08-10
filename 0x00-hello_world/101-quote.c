600812f97bf1d83df681112083b7412854055ee4
JIhan Fakhr <jihanahmed41@gmail.com>
2023-08-10 08:19:57 -0700

    adding qoute file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *discription: printing output without using printf or puts
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+char *buffer = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write (1, buffer, 59);
+return (1);
+}

2cc025f4439f05d56e3f3ebb8f500d9a28959960
JIhan Fakhr <jihanahmed41@gmail.com>
2023-08-11 06:38:37 -0700

    adding print alphabet file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphapets
+ *
+ * Description: program will prints ll alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	do {
+		putchar(c);
+		c = c + 1;
+	} while (c <= 'z');
+	putchar('\n');
+
+	return (0);
+}

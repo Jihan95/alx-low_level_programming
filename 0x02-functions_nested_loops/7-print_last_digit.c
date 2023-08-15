124e808de9c95b90d0410851cb9238fc2214329d
JIhan Fakhr <jihanahmed41@gmail.com>
2023-08-15 13:13:14 -0700

    adding print last digit file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_last_digit - returns the last digit of passed integer
+ *@r: the passed integer
+ *
+ *Return: the last digit of passed integer
+ */
+
+int print_last_digit(int r)
+{
+	int n = r % 10;
+
+	if ( n < 0)
+		n = n * -1;
+	_putchar(n + '0');
+	return (n);
+}

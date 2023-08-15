68caf7d83b5b80a9c45fb8d280e19bc994d37714
JIhan Fakhr <jihanahmed41@gmail.com>
2023-08-15 13:22:45 -0700

    adding add file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *add - adds two passed integers
+ *@n1: the first passed integer
+ *@n2: the second passed integer
+ *
+ *Return: the result of addition operation
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}

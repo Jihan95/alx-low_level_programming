9e2e40c758d0708c7075afc944d640fffcbecda1
JIhan Fakhr <jihanahmed41@gmail.com>
2023-08-15 11:35:58 -0700

    adding is alpha file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _isalpha-  checks if input is alphabet;
+ *@c : the ascii code of the passed char
+ *
+ * Return: (1) if c is a letter and (0) otherwise
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}

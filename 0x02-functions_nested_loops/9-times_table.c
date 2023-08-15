61dca709cb0b1df1d2650f0415f02c213414621c
JIhan Fakhr <jihanahmed41@gmail.com>
2023-08-15 14:53:59 -0700

    adding time tables file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,14 @@
 
 void times_table(void)
 {
-	int i, j, cross;
+	int i, j, cross, cross_next;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{	cross = i * j;
-			if (cross <= 9)
+			cross_next = i * (j + 1);
+			if (cross_next <= 9)
 			{	_putchar(cross + '0');
 				if (j < 9)
 				{

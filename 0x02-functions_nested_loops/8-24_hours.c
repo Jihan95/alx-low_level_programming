bb63ecc99ad69cb1b90b0b5c8f9af0b264c870a4
JIhan Fakhr <jihanahmed41@gmail.com>
2023-08-16 05:45:39 -0700

    updating 24 hours file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- * void jack_bauer - prints every minute of the day of Jack Bauer
- *
+ * jack_bauer - prints every minute of the day of Jack Bauer
  */
 
 void jack_bauer(void)
-{
-	int i, j, k, l;
+{	int i, j, k, l;
 
 	for (i = 0; i <= 2; i++)
 	{
@@ -18,17 +16,12 @@ void jack_bauer(void)
 				for (k = 0; k <= 5; k++)
 				{
 					for (l = 0; l <= 9; l++)
-					{
-						_putchar(i + '0');
+					{	_putchar(i + '0');
 						_putchar(j + '0');
 						_putchar(':');
 						_putchar(k + '0');
 						_putchar(l + '0');
-						_putchar('\n');
-					}
-				}
-			}
-		}
+						_putchar('\n'); } } } }
 		else
 		{
 			for (j = 0; j <= 3; j++)
@@ -36,16 +29,11 @@ void jack_bauer(void)
 				for (k = 0; k <= 5; k++)
 				{
 					for (l = 0; l <= 9; l++)
-					{
-						_putchar(i + '0');
+					{	_putchar(i + '0');
 						_putchar(j + '0');
 						_putchar(':');
 						_putchar(k + '0');
 						_putchar(l + '0');
-						_putchar('\n');
-					}
-				}
-			}
-		}
+						_putchar('\n'); } } } }
 	}
 }

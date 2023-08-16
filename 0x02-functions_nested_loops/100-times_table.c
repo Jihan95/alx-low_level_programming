bd8504493e269b4374dd0555d2b450ef9e92d147
JIhan Fakhr <jihanahmed41@gmail.com>
2023-08-16 08:18:16 -0700

    adding 100-time tables file

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ *@n: the required time table
+ */
+
+void print_times_table(int n)
+{
+	int i, j, cross, cross_next;
+
+	if (n > 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{	cross = i * j;
+				cross_next = i * (j + 1);
+				if (cross < 9 && cross_next <= 9)
+				{
+					_putchar(cross + '0');
+					if (j < n)
+					{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' '); }
+				}
+				else if (((cross < 9) && (cross_next > 9)) || ((cross > 9) && (cross < 99) && (cross_next <= 99)))
+				{
+					_putchar(cross / 10 + '0');
+					_putchar(cross % 10 + '0');
+					if (j < n)
+					{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' '); } }
+				else
+				{
+					_putchar(cross / 10 + '0');
+					_putchar(cross % 10 + '0');
+					_putchar(cross % 100 + '0');
+					if (j < n)
+					{
+						_putchar(',');
+						_putchar(' '); }
+				}
+			}
+		_putchar('\n'); }
+	}
+	else
+		_putchar('\n');
+}

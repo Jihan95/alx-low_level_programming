e61ffe965aabba744b0e470cbbdbe8513eb740a2
JIhan Fakhr <jihanahmed41@gmail.com>
2023-08-17 08:09:03 -0700

    updating print numbers file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,11 +11,9 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
-	}
+		n = n * -1; }
 	if (n >= 0 && n <= 9)
-	{	_putchar(n + '0');
-	}
+		_putchar(n + '0');
 	else if (n >= 10 && n <= 99)
 	{
 		_putchar(n / 10 + '0');
@@ -28,6 +26,13 @@ void print_number(int n)
 	else if (n >= 1000 && n <= 9999)
 	{
 		_putchar(n / 1000 + '0');
+		_putchar((n % 1000) / 100 + '0');
+		_putchar((n % 100) / 10 + '0');
+		_putchar(n % 10 + '0'); }
+	else if (n >= 10000 && n <= 99999)
+	{
+		_putchar(n / 10000 + '0');
+		_putchar((n % 10000) / 1000 + '0');
 		_putchar((n % 1000) / 100 + '0');
 		_putchar((n % 100) / 10 + '0');
 		_putchar(n % 10 + '0'); }

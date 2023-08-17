972524a9a4df6e48227abb03c0965e95e6111d94
JIhan Fakhr <jihanahmed41@gmail.com>
2023-08-17 07:56:54 -0700

    adding print numbers file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Description: finds and prints the largest prime factor of 
+ * the number 612852475143, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long long int primeFactor, n = 612852475143;
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - print passed number only using _putchar
+ *@n : the number required to be printed
+ */
+
+void print_number(int n)
+
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	if (n >= 0 && n <= 9)
+	{	_putchar(n + '0');
+	}
+	else if (n >= 10 && n <= 99)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');	}
+	else if (n >= 100 && n <= 999)
+	{
+		_putchar(n / 100 + '0');
+		_putchar((n % 100) / 10 + '0');
+		_putchar(n % 10 + '0'); }
+	else if (n >= 1000 && n <= 9999)
+	{
+		_putchar(n / 1000 + '0');
+		_putchar((n % 1000) / 100 + '0');
+		_putchar((n % 100) / 10 + '0');
+		_putchar(n % 10 + '0'); }
+}

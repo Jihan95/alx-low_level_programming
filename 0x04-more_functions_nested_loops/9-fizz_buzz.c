1198889a72f28cea34cbe4cd36c1d51aaf026a99
JIhan Fakhr <jihanahmed41@gmail.com>
2023-08-17 05:44:42 -0700

    updating fizz buzz file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,10 +19,12 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 15 != 0)
 			printf("Fizz ");
-		else if (i % 5 == 0 && i % 15 != 0)
+		else if (i % 5 == 0 && i % 15 != 0 && i != 100)
 			printf("Buzz ");
 		else if (i % 15 == 0)
 			printf("FizzBuzz ");
+		else if (i == 100)
+			printf("Buzz");
 		else
 			printf("%d ", i);
 	}

a4a307031c0c56243547087770f9029e19fee422
JIhan Fakhr <jihanahmed41@gmail.com>
2023-08-21 06:19:39 -0700

    adding puts2 file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - rints every other character of a string.
+ * @str: passed string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i = i + 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}

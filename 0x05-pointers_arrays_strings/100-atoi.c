9b3da401a2cf5ef385d9c02e9f05002df31f6a14
JIhan Fakhr <jihanahmed41@gmail.com>
2023-08-21 08:39:23 -0700

    adding atoi file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: passed string
+ *
+ * Return: the numerical value of passed string
+ */
+
+int _atoi(char *s)
+{
+	int num = 0, i, sign = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			num = num * 10 + (s[i] - 48); }
+		else if (s[i - 1] == 45)
+			sign = -1;
+		else if (s[i + 1] <= 48 && s[i + 1] >= 57)
+			break;
+		else
+			continue;
+	}
+	return (sign * num);
+}

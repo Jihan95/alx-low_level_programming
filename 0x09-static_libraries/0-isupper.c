77b4eaed4e935b32e0f5544549714344fe0e5d84
Jihan Fakhr <jihanahmed41@gmail.com>
2023-10-02 05:04:01 -0700

    adding static library file

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper -  checks if input is uppercase
+ *@c : the ascii code of the passed char
+ *
+ * Return: (1) if c is lower case and (0) otherwise
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: passed string
+ *
+ * Return: the numerical value of passed string
+ */
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int i, sign = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			num = num * 10 + (s[i] - 48);
+			if (s[i + 1] == ' ')
+				break;	}
+		else if (s[i] == 45)
+			sign = sign * -1;
+		else if (s[i + 1] <= 48 && s[i + 1] >= 57)
+			break;
+	}
+	return (sign * num);
+}

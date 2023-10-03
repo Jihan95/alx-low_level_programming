c2bbe3ed50b4bb739c743b8cedc97bd10f53f5f1
Jihan Fakhr <jihanahmed41@gmail.com>
2023-10-03 10:22:57 -0700

    adding 2-str_concat.c file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,11 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 	int i, j = 0, k = 0, str_length = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	while (*(s1 + k) != '\0')
 	{

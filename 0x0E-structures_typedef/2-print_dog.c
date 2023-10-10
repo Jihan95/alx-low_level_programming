60617c5cf88c25850652949a4099f2979676f406
Jihan Fakhr <jihanahmed41@gmail.com>
2023-10-10 05:46:35 -0700

    adding 2-print_dog.c file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+#define MISSING "(nil)"
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to a struct type variable
+ */
+
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", (d->name != NULL) ? d->name : MISSING);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : MISSING);
+}

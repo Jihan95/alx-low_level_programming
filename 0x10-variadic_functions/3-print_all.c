71db9b9a033873006eeac181b4cc6fce24887a11
Jihan Fakhr <jihanahmed41@gmail.com>
2023-10-13 06:17:50 -0700

    updating 3-print_all.c file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,47 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - prints anything
- * @format: is a list of types of arguments passed to the function
- */
+  * print_all - prints passed arguments
+  * @format: is a list of types of arguments passed to the function
+  */
 
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	char c;
-	int i;
-	float f;
-	char *s;
-	char current_format;
-	int printed = 0, k = 1;
+	size_t formatlen;
+	char *formatCpy, *str;
+	va_list allargs;
 
-	va_start(args, format);
-	current_format = *format;
-	while ((current_format) != '\0')
+	formatlen = strlen(format);
+	formatCpy = (char *)malloc(formatlen + 1);
+	if (formatCpy != NULL)
+		strcpy(formatCpy, format);
+	va_start(allargs, format);
+	while (*formatCpy)
 	{
-		if (printed >= 1)
+		switch (*formatCpy)
+		{
+			case 'c':
+				printf("%c", va_arg(allargs, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(allargs, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(allargs, double));
+				break;
+			case 's':
+				str = va_arg(allargs, char *);
+				if (str == NULL)
+					printf("(nil)");
+				printf("%s", str);
+				break;
+			default:
+				formatCpy++;
+				continue; }
+		if (*(formatCpy + 1))
 			printf(", ");
-		if (current_format == 'c')
-		{
-			c = va_arg(args, int);
-			printf("%c", c);
-			printed++; }
-		else if (current_format == 'i')
-		{
-			i = va_arg(args, int);
-			printf("%d", i);
-			printed++; }
-		else if (current_format == 'f')
-		{
-			f = va_arg(args, double);
-			printf("%f", f);
-			printed++; }
-		else if (current_format == 's')
-		{
-			s = va_arg(args, char *);
-			if (s == NULL)
-				printf("(nil)");
-			else
-			printf("%s", s);
-			printed++; }
-	current_format = *(format + k);
-	k++; }
-	va_end(args);
-	printf("\n"); }
+		formatCpy++;
+	}
+	printf("\n");
+	va_end(allargs);
+}

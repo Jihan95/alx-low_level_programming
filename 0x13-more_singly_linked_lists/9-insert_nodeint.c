a47a52f0359cce94536707a59bed063d9e1ef91c
Jihan Ahmed <jihanahmed41@gmail.com>
2023-10-23 12:00:48 -0700

    updating 9-insert_nodeint.c file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,6 +17,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if ((size_t)idx >= len)
 		return (NULL);
+	if (head == NULL || *head == NULL)
+		return (NULL);
 	newNode = (listint_t *)malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);

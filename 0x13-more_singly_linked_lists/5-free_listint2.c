dca72c935fdbd7e201475f90d1f893ed118d195a
Jihan Ahmed <jihanahmed41@gmail.com>
2023-10-23 11:25:10 -0700

    updating 5-free_listint2.c file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,12 +7,15 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *temp = *head;
 
+	if(head == NULL || *head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
+	*head = NULL;
 }

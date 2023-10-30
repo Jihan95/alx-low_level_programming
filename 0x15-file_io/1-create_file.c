4cecdd29c03cbbd715c033e4000742128a4b0479
Jihan Ahmed <jihanahmed41@gmail.com>
2023-10-30 07:40:11 -0700

    adding 1-create_file.c file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t wBytes;
 
-	if (filename == NULL || text_content == NULL)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)

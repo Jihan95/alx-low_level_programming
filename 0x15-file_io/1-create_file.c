6987e7d06bf0bb88ac4f12eeddd99cdf2d061e7d
Jihan Ahmed <jihanahmed41@gmail.com>
2023-10-30 07:48:28 -0700

    adding 1-create_file.c file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	wBytes = write(fd, text_content, strlen(text_content));
 	if (wBytes == -1)
 		return (-1);

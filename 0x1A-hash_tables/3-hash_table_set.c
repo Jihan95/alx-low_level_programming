e57b18cd73d8820fc33200ea0a49a1247b973f49
Jihan Ahmed <jihanahmed41@gmail.com>
2023-12-22 03:16:44 -0800

    adding file no.3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,50 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: the hash table you want to add or update the key/value to
+ * @key: is the key. key can not be an empty string
+ * @value: value is the value associated with the key.
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *item, *current_item;
+	unsigned long int index;
+
+	if (ht == NULL)
+		return(0)
+	item = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (item == NULL || key == NULL)
+		return (0);
+	item->key = strdup(key);
+	if (item->key == NULL)
+	{
+		free(item);
+		return (0); }
+	item->value = strdup(value);
+	if (item->value == NULL)
+	{
+		free(item);
+		free(item->key);
+		return (0); }
+	item->next = NULL;
+	index = key_index((const unsigned char *)key, ht->size);
+	current_item = ht->array[index];
+	if (current_item == NULL)
+	{
+		ht->array[index] = item;
+		return (1); }
+	if (strcmp(current_item->key, key) == 0)
+	{
+		strdup(current_item->value, value);
+		free(item->key);
+		free(item->value);
+		free(item);
+		return (1); }
+	item->next = current_item;
+	ht->array[index] = item;
+	return (1);
+}
